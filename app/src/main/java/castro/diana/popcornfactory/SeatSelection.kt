c66e92756b6041e196166afd30095e88532c6698
Diana Castro <dianae.castro@gmail.com>
2021-03-15 15:50:17 -0700

    little fix on seat state changing

--- app/src/main/java/castro/diana/popcornfactory/SeatSelection.kt
+++ app/src/main/java/castro/diana/popcornfactory/SeatSelection.kt
@@ -2,6 +2,7 @@ package castro.diana.popcornfactory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.RadioButton
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_seat_selection.*
@@ -15,6 +16,7 @@ class SeatSelection : AppCompatActivity() {
         var positionMov = -1
         var ns = 0
         var id: Int = 0
+        var radio: RadioButton
 
         if(bundle!=null) {
             tv_titleSeats.setText(bundle.getString("name"))
@@ -27,34 +29,42 @@ class SeatSelection : AppCompatActivity() {
 
             if(row1.checkedRadioButtonId != -1){
                 id = row1.checkedRadioButtonId
-                var radio: RadioButton = findViewById(id)
+                radio = findViewById(id)
                 if (radio.isChecked){
                     radio.setBackgroundResource(R.drawable.radio_disabled)
                     Toast.makeText(this, radio.text, Toast.LENGTH_SHORT).show()
+                    radio.isChecked = false
+                    radio.isEnabled = false
                     ns -1
                 }
             }else if (row2.checkedRadioButtonId != -1){
                 id = row2.checkedRadioButtonId
-                var radio: RadioButton = findViewById(id)
+                radio = findViewById(id)
                 if (radio.isChecked){
                     radio.setBackgroundResource(R.drawable.radio_disabled)
                     Toast.makeText(this, radio.text, Toast.LENGTH_SHORT).show()
+                    radio.isChecked = false
+                    radio.isEnabled = false
                     ns -1
                 }
             }else if(row3.checkedRadioButtonId != -1){
                 id = row3.checkedRadioButtonId
-                var radio: RadioButton = findViewById(id)
+                radio = findViewById(id)
                 if (radio.isChecked){
                     radio.setBackgroundResource(R.drawable.radio_disabled)
                     Toast.makeText(this, radio.text, Toast.LENGTH_SHORT).show()
+                    radio.isChecked = false
+                    radio.isEnabled = false
                     ns -1
                 }
             }else if(row4.checkedRadioButtonId != -1){
                 id = row4.checkedRadioButtonId
-                var radio: RadioButton = findViewById(id)
+                radio = findViewById(id)
                 if (radio.isChecked){
                     radio.setBackgroundResource(R.drawable.radio_disabled)
                     Toast.makeText(this, radio.text, Toast.LENGTH_SHORT).show()
+                    radio.isChecked = false
+                    radio.isEnabled = false
                     ns -1
                 }
             }
